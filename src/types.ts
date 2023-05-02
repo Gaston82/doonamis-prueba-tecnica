87edf40f6de5098e7456605dd324af9c992601d5
Gaston82 <cabezaartaza@hotmail.com>
2023-05-02 23:41:27 +0200

    Add image and data properties to Show interface

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,13 @@ export interface Show {
   name: string;
   vote_average: number;
   overview: string;
+  first_air_date: string;
+  poster_path: string;
 }
 
 export type Shows = Show[];
+
+export interface ApiResponse {
+  page: number;
+  results: Shows;
+}
