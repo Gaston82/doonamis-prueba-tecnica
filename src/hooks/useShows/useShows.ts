4272b4c77f40bcefab82ca8a9e9ecbd25f9072da
Gaston82 <cabezaartaza@hotmail.com>
2023-05-03 15:53:56 +0200

    Create getSeriesById function

--- src/hooks/useShows/useShows.ts
+++ src/hooks/useShows/useShows.ts
@@ -1,13 +1,16 @@
-import { loadShowsActionCreator } from "../../store/feature/show/showSlice";
+import {
+  loadShowActionCreator,
+  loadShowsActionCreator,
+} from "../../store/feature/show/showSlice";
 import { useAppDispatch } from "../../store/hooks";
-import { type ApiResponse, type Shows } from "../../types";
+import { type ShowStructure, type ApiResponse, type Shows } from "../../types";
 
 const useShows = () => {
   const dispatch = useAppDispatch();
 
   const getAllShows = async () => {
     try {
-      const response = await fetch(import.meta.env.VITE_BASE_URL);
+      const response = await fetch(import.meta.env.VITE_SERIES_URL);
       const shows = (await response.json()) as ApiResponse;
 
       dispatch(loadShowsActionCreator(shows.results));
@@ -16,7 +19,18 @@ const useShows = () => {
     }
   };
 
-  return { getAllShows };
+  const getSeriesById = async () => {
+    try {
+      const response = await fetch(import.meta.env.VITE_SERIES_DETAILS_URL);
+      const show = (await response.json()) as ShowStructure;
+
+      dispatch(loadShowActionCreator(show));
+    } catch (error) {
+      return (error as Error).message;
+    }
+  };
+
+  return { getAllShows, getSeriesById };
 };
 
 export default useShows;
