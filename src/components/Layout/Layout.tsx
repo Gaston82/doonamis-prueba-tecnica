791f4b481e85aaec4ce9c553882aa085de2ab779
Gaston82 <cabezaartaza@hotmail.com>
2023-05-08 08:12:18 +0200

    Implement ui reducer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,22 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import { useAppSelector } from "../../store/hooks";
+import Spinner from "../Spinner/Spinner";
 
-const Layout = (): JSX.Element => (
-  <>
-    <Header />
-    <Outlet />
-    <Footer />
-  </>
-);
+const Layout = (): JSX.Element => {
+  const isLoading = useAppSelector((state) => state.ui.isLoading);
+
+  return (
+    <>
+      <Header />
+      <>
+        {isLoading && <Spinner />}
+        <Outlet />
+      </>
+      <Footer />
+    </>
+  );
+};
 
 export default Layout;
